b9c58044918e940fbecb2eb9253a22fcfcec59d2
Shapon Sheikh <shsheikhbd@gmail.com>
2024-09-10 14:46:53 +0600

    removed few projects

--- data.js
+++ data.js
@@ -36,40 +36,6 @@ export const skills = [
 ];
 
 export const projects = {
-    webProjects: [
-        {
-            projectName: "Programming Diaries",
-            image: "images/programmingdiaries.png",
-            summary:
-                "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-            preview: "https://programmingdiaries.herokuapp.com/",
-            techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
-        },
-        {
-            projectName: "Find Your Bank",
-            image: "images/findyourbank.png",
-            summary:
-                "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-            preview: "https://clever-fermi-0d5d76.netlify.app",
-            techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
-        },
-        {
-            projectName: "Web Portfolio",
-            image: "images/portfolio.png",
-            summary:
-                "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-            preview: "https://github.com/vinaysomawat/Travographer-Portal",
-            techStack: ["HTML", "Bootstrap", "JavaScript"],
-        },
-        {
-            projectName: "Resume Builder",
-            image: "images/resume-builder.png",
-            summary:
-                "Browser based editor to build and download Resumes in a customizable templates.",
-            preview: "https://vinaysomawat.github.io/Resume-Builder",
-            techStack: ["HTML", "Bootstrap", "JavaScript"],
-        },
-    ],
     softwareProjects: [
         {
             projectName: "Pizza Ordering ChatBot",
@@ -94,24 +60,6 @@ export const projects = {
                 "GUI to transfer data to excel sheets and generate bills on the local shops.",
             preview: "https://github.com/vinaysomawat/Bill-Generator",
             techStack: ["Tkinter", "Openxlpy", "Python"],
-        },
-    ],
-    androidProjects: [
-        {
-            projectName: "NITW-CSE",
-            image: "images/nitwcse.jpg",
-            summary:
-                "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-            preview: "https://github.com/vinaysomawat/NITW-CSE",
-            techStack: ["JAVA", "XML", "Android"],
-        },
-        {
-            projectName: "CareerHigh-App",
-            image: "images/carrerhigh.png",
-            summary:
-                "The Application display the webpages of website careerhigh.in in android devices.",
-            preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-            techStack: ["JAVA", "XML", "Android"],
         },
     ],
     freelanceProjects: [
